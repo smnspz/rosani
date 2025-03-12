a3d4bb66e3be45795ef35c52927e1b408886b4f9
Simone Rosani <rsn.smn@gmail.com>
2025-03-12 18:07:47 +0100

    add i18n

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,33 @@
 "use client";
 
-import { useState } from "react";
 import TextFiglet from "./components/text-figlet";
 import Link from "next/link";
+import { useTranslations } from "next-intl";
 
 export default function Home() {
-  const [text] = useState("rosani");
+  const t = useTranslations("Homepage");
 
   return (
     <div className="font-mono flex flex-col items-center justify-center p-4">
       <TextFiglet
-        text={text}
+        text={"rosani"}
         animate={true}
         animationSpeed={0.4}
         minHeight="180px"
         className="text-center"
       />
-      <p>Hello visitor, i&apos;m Simone (Rosani), a web and mobile developer</p>
+      <p>{t("about.hello")}</p>
       <p>
-        I love clean and simple UIs, but i&apos;m also intrigued by{" "}
-        <Link
-          href="https://sabrinas.space/"
-          className="hover:underline underline sm:no-underline underline-offset-5"
-        >
-          the way japanese people prefere their websites
-        </Link>
+        {t.rich("about.love", {
+          link: (chunks) => (
+            <Link
+              href="https://sabrinas.space"
+              className="hover:underline underline sm:no-underline underline-offset-4"
+            >
+              {chunks}
+            </Link>
+          ),
+        })}
       </p>
     </div>
   );
